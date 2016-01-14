8e72c8b38ce66a86bde5d915d0fb748fe1784a04
Jure Mirat <georgemirat3@yahoo.com>
2016-01-14 18:17:28 +0100

    v0.3: - sync fix

---
+++ point3ri Alpha 0.1/point3ri/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace point3ri.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin/Admin
+        [Authorize(Roles = "Admin")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
