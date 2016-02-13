decbb81e3ef723317d93a361ab8c8e7e5663c3fb
Jure Mirat <georgemirat3@yahoo.com>
2016-02-13 23:08:55 +0100

    Jquery skripte i view modeli za dynamic dropdown liste.

---
+++ point3ri Alpha/point3ri Alpha/Models/ViewModel/DanTerminViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace point3ri_Alpha_0._51.Models.ViewModel
+{
+    class DanTerminViewModel
+    {
+        public List<DanTermini> DanTerminiList = new List<DanTermini>();
+        public int SelectedDanTerminiID { get; set; }
+        public IEnumerable<SelectListItem> DanTerminiIEnum
+        {
+            get
+            {
+                return new SelectList(DanTerminiList, "ID", "Termin");
+            }
+        }
+
+
+    }
+}
