396fa2113b55831483acc00decf087c2887d0b5c
Jure Mirat <georgemirat3@yahoo.com>
2016-02-28 15:40:54 +0100

    Dodana funkcija za trazenje trenutnih slobodnih termina.

--- point3ri Alpha/point3ri Alpha/Controllers/HomeController.cs
+++ point3ri Alpha/point3ri Alpha/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using point3ri_Alpha_0._51.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,8 @@ namespace point3ri_Alpha_0._51.Controllers
 {
     public class HomeController : Controller
     {
+        private point3ri db = new point3ri();
+
         public ActionResult Index()
         {
             return View();
@@ -21,6 +24,28 @@ namespace point3ri_Alpha_0._51.Controllers
             ViewBag.Message = "Your application description page.";
 
             return View();
+        }
+
+        // funkcija za trazenje slobodnih termina
+        public int BrojSlobodnihRezervacija(int KategorijaOpreme)
+        {
+            int brojSlobodnih = 0;
+            int trenutnoZauzeti = 0;
+            int brojOpreme = db.Opremas.Count(o => o.KategorijaOpremeID == KategorijaOpreme);
+
+            foreach (Rezervacija rezervacija in db.Rezervacijas)
+            {
+                if (rezervacija.Oprema.KategorijaOpremeID == KategorijaOpreme &&
+                    rezervacija.RezervacijaAktivna == true &&
+                    rezervacija.DatumRezervacije == DateTime.Today &&
+                    rezervacija.DanTermini.Termin >= DateTime.Now.TimeOfDay)
+                {
+                    trenutnoZauzeti += 1;
+                }
+
+            }
+            brojSlobodnih = brojOpreme - trenutnoZauzeti;
+            return brojSlobodnih;
         }
     }
 }
